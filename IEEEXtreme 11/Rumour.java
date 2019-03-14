fd71ab5a41c7c514713800737c46e2bd07eeb634
ntul <48542035+ntul@users.noreply.github.com>
2019-03-14 01:07:56 -0400

    Update Rumour.java

--- IEEEXtreme 11/Rumour.java
+++ IEEEXtreme 11/Rumour.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Rumour {
+public class Main {
  
     
   public static void main(String[] args) throws IOException {
